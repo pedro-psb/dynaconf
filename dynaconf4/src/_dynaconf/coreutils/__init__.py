d16c34c3dcbb75c630ee18a7f06dc17b3eafa594
Pedro Brochado <pedropsb95@gmail.com>
2024-10-19 15:06:29 -0300

    Rename coreutils files to follow {module}_{details} convention

--- dynaconf4/src/_dynaconf/coreutils/__init__.py
+++ dynaconf4/src/_dynaconf/coreutils/__init__.py
@@ -1,5 +1,5 @@
-from .apply_merge_tree import apply_merge_tree
-from .create_merge_tree import create_merge_tree
+from .merge_tree_apply import apply_merge_tree
+from .merge_tree_create import create_merge_tree
 from .tokenize import tokenize
 from .load import load
 from .token_registry import TokenRegistry
